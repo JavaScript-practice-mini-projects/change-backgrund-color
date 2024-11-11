5c533f13be0f8420d77f1e08648579f8d47bb74d
mhrhabib-dev <mhrhabib77232@gmail.com>
2024-11-11 12:07:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//step
+// step 1 onload handler
+window.onload = () => {
+    main();
+}
+function main(){
+    const colorContainer = document.getElementById('colorContainer');
+    const changeButton = document.getElementById('changeButton');
+    const copyButton = document.getElementById('copyButton');
+}
