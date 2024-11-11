55d39a8f2f252670d093fa4dec4f5b517cebd4f6
mhrhabib-dev <mhrhabib77232@gmail.com>
2024-11-11 21:40:15 +0600

    updade and add more

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ function main(){
     const colorContainer = document.getElementById('colorContainer');
     const changeButton = document.getElementById('changeButton');
     const copyButton = document.getElementById('copyButton');
+    const colorShow = document.getElementById('colorShow');
 
     //changeButton function call
    changeButton.addEventListener('click', function () {
         const bgColor = GeneratorRGBColor();
         colorContainer.style.backgroundColor = bgColor;
+        colorShow.value = bgColor;
+   })
+
+   // color code copy button 
+   copyButton.addEventListener('click', function(){
+    navigator.clipboard.writeText(colorShow.value);
+    generateToastMsg(`${colorShow.value} Copied!`);
+
+   
    })
 }
 
@@ -23,5 +33,24 @@ function GeneratorRGBColor() {
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
     
-    return `rgb(${red}, ${green}, ${blue})`
+    return `rgb(${red}, ${green}, ${blue})`;
 }
+
+// generateToastMsg
+function generateToastMsg(msg){
+    const copiedContainer = document.createElement("div");
+    copiedContainer.textContent = msg;
+    copiedContainer.className = 'toastMessage toastMessage-slide-in';
+
+  
+    copiedContainer.addEventListener('click', function() {
+        copiedContainer.classList.remove('toastMessage-slide-in');
+        copiedContainer.classList.add('toastMessage-slide-out')
+        copiedContainer.addEventListener('animationend', function() {
+            copiedContainer.remove()
+        })
+    })
+
+    document.body.appendChild(copiedContainer);
+}
+
